21f6041e58e4c6d92629c8cfc3f4c13766b29a74
Morten Manzelmann <morten.manzelmann@ministrygroup.de>
2020-03-26 13:19:27 +0100

    server delivers all it needs to start developing

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
- 
+import * as path from 'path';
+
 const app = express();
+app.use(express.static('./'));
  
  app.get('/', (request, response) => {
-     response.send('Hello world!');
+     response.sendFile(__dirname + '/index.html');
  });
  
 app.listen(3000);
