9bcb97ab7a0c5ba75546e1d0bce435ec0efd999f
Morten Manzelmann <morten.manzelmann@ministrygroup.de>
2020-10-05 23:00:02 +0200

    refactored sctions

--- src/frontend/actions/index.ts
+++ src/frontend/actions/index.ts
@@ -1,87 +1,87 @@
 import { Action, Dispatch } from 'redux';
-import ScoutRecommendationList from "../interfaces/scout-recommendation-list.interface";
-import ScoutRecommendationApi from '../services/api/scout-recommendation.api';
-import { ScoutRecommendation } from  '../interfaces/scout-recommendation.interface'
+import MessageList from "../interfaces/message-list.interface";
+import MessageApi from '../services/api/message.api';
+import { Message } from  '../interfaces/message.interface'
 
-export const ACTION_RECOMMENDATIONS_FETCH = 'RECOMMEDNATIONS_FETCH';
-export const ACTION_RECOMMENDATIONS_FETCH_SUCCESS = 'RECOMMEDNATIONS_FETCH_SUCCESS';
-export const ACTION_RECOMMENDATIONS_FETCH_ERROR = 'RECOMMEDNATIONS_FETCH_ERROR';
+export const ACTION_MESSAGE_FETCH = 'MESSAGE_FETCH';
+export const ACTION_MESSAGE_FETCH_SUCCESS = 'MESSAGE_FETCH_SUCCESS';
+export const ACTION_MESSAGE_FETCH_ERROR = 'MESSAGE_FETCH_ERROR';
 export const ACTION_POST_MESSAGE = 'POST_MESSAGE';
 
 export function isAction<A extends Action>(action: Action, type: string): action is A {
   return action.type === type;
 }
 
-export interface IActionRecommendationsFetch extends Action {
-  type: 'RECOMMEDNATIONS_FETCH'
+export interface IActionMessageFetch extends Action {
+  type: 'MESSAGE_FETCH'
 }
 
-export interface IActionRecommendationsFetchSuccess extends Action {
-  type: 'RECOMMEDNATIONS_FETCH_SUCCESS',
-  scoutRecommendations: ScoutRecommendationList
+export interface IActionMessageFetchSuccess extends Action {
+  type: 'MESSAGE_FETCH_SUCCESS',
+  scoutMessage: MessageList
 }
 
-export interface IActionRecommendationsFetchError extends Action {
-  type: 'RECOMMEDNATIONS_FETCH_ERROR',
+export interface IActionMessageFetchError extends Action {
+  type: 'MESSAGE_FETCH_ERROR',
   errorMessage: string
 }
 
 export interface IActionPost extends Action {
   type: 'POST_MESSAGE',
-  message: ScoutRecommendation
+  message: Message
 }
 
-export type AppActions = IActionRecommendationsFetch | IActionRecommendationsFetchSuccess | IActionRecommendationsFetchError;
+export type AppActions = IActionMessageFetch | IActionMessageFetchSuccess | IActionMessageFetchError;
 
-function dispatchFetchRecommendationsProgress(): IActionRecommendationsFetch {
+function dispatchFetchMessageProgress(): IActionMessageFetch {
   return {
-    type: ACTION_RECOMMENDATIONS_FETCH
+    type: ACTION_MESSAGE_FETCH
   }
 }
 
-function dispatchFetchRecommendationsSuccess(scoutRecommendations: ScoutRecommendationList): IActionRecommendationsFetchSuccess {
+function dispatchFetchMessageSuccess(scoutMessage: MessageList): IActionMessageFetchSuccess {
   return {
-    type: ACTION_RECOMMENDATIONS_FETCH_SUCCESS,
-    scoutRecommendations
+    type: ACTION_MESSAGE_FETCH_SUCCESS,
+    scoutMessage
   }
 }
 
-function dispatchFetchRecommendationsError(error: Error): IActionRecommendationsFetchError {
+function dispatchFetchMessageError(error: Error): IActionMessageFetchError {
   return {
-    type: ACTION_RECOMMENDATIONS_FETCH_ERROR,
+    type: ACTION_MESSAGE_FETCH_ERROR,
     errorMessage: error.message
   }
 }
 
-function dispatchPostMessage(message: ScoutRecommendation): IActionPost{
+function dispatchPostMessage(message: Message): IActionPost{
   return {
 type: ACTION_POST_MESSAGE,
 message
   }
 }
 
-export function actionFetchRecommendations(){
+export function actionFetchMessage(){
   return (dispatch: Dispatch) => {
-    // dispatch(dispatchFetchRecommendationsProgress());
-    return ScoutRecommendationApi.getAll()
+    // dispatch(dispatchFetchMessageProgress());
+    return MessageApi.getAll()
     .then((recommendations) => {
-      return dispatch(dispatchFetchRecommendationsSuccess(recommendations));
+      return dispatch(dispatchFetchMessageSuccess(recommendations));
     })
     .catch((error: Error) => {
-      return dispatch(dispatchFetchRecommendationsError(error));
+      return dispatch(dispatchFetchMessageError(error));
     });
   };
 }
 
-export function actionPostMessage(message: ScoutRecommendation){
+export function actionPostMessage(message: Message){
   return (dispatch: Dispatch) => {
-    ScoutRecommendationApi.post(message);
-    return ScoutRecommendationApi.getAll()
+    MessageApi.post(message);
+    return MessageApi.getAll()
     .then((recommendations) => {
-      return dispatch(dispatchFetchRecommendationsSuccess(recommendations));
+      return dispatch(dispatchFetchMessageSuccess(recommendations));
     })
     .catch((error: Error) => {
-      return dispatch(dispatchFetchRecommendationsError(error));
+      return dispatch(dispatchFetchMessageError(error));
     });
   }
 }
--- src/frontend/interfaces/message.interface.ts
+++ src/frontend/interfaces/message.interface.ts
@@ -1,4 +1,4 @@
-export interface ScoutRecommendation {
+export interface Message {
   title: string;
   description: string;
   author: string;
